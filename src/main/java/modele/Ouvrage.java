6f75b444516f6ec754f48b049a86436612cbbd98
jade <jadeclemenceaub@hotmail.com>
2022-03-04 14:17:22 +0100

    Création de la classe Ouvrage permettant la gestion des ouvrages de la bibliothèque

--- src/main/java/modele/Ouvrage.java
+++ src/main/java/modele/Ouvrage.java
@@ -1,8 +1,12 @@
 package modele;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Ouvrage {
+    public Ouvrage(Integer num, String ISBN, String titreOuvrage, String nomEditeur, String nomAuteur) {
+
+    }
 }
