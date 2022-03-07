69a3c9c354784ce9f7abe2e9f4b8797fe4acb127
jade <jadeclemenceaub@hotmail.com>
2022-03-07 16:28:52 +0100

    Rédaction de la calsse modèle Ouvrage (constructeur et getter)

--- src/main/java/modele/Ouvrage.java
+++ src/main/java/modele/Ouvrage.java
@@ -2,11 +2,36 @@ package modele;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
-public class Ouvrage {
-    public Ouvrage(Integer num, String ISBN, String titreOuvrage, String nomEditeur, String nomAuteur) {
 
+public class Ouvrage implements Serializable {
+    private final Integer ibsn;
+    private String titre;
+    private String nomEditeur;
+    private Date dateParution;
+
+    public Ouvrage(Integer ibsn, String titre, String nomEditeur, Date dateParution) {
+        this.ibsn = ibsn;
+        this.titre = titre;
+        this.nomEditeur = nomEditeur;
+        this.dateParution = dateParution;
+    }
+
+    public Integer getIbsn() {
+        return ibsn;
+    }
+
+    public String gettitre() {
+        return titre;
+    }
+
+    public String getNomEditeur() {
+        return nomEditeur;
+    }
+
+    public Date getdateParution() {
+        return dateParution;
     }
 }
+
