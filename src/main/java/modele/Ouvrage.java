93b745cea7e2e43e9488406a2a21576dc42f2fd8
jade <jadeclemenceaub@hotmail.com>
2022-03-17 10:56:00 +0100

    Modification de la classe ouvrage avec ajout des attributs manquants

--- src/main/java/modele/Ouvrage.java
+++ src/main/java/modele/Ouvrage.java
@@ -2,7 +2,9 @@ package modele;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class Ouvrage implements Serializable {
@@ -12,13 +14,92 @@ public class Ouvrage implements Serializable {
     private final String Auteur;
     private final String nomEditeur;
     private final LocalDate dateParution;
+    private final int dernierExemplaire;
+    private final Set <Exemplaire> exemplaires;
 
-    public Ouvrage(String ibsn, String titre, String Auteur, String nomEditeur, LocalDate dateParution) {
+    public Ouvrage(String ibsn, String titre, String Auteur, String nomEditeur, LocalDate dateParution, int dernierExemplaire) {
         this.ibsn = ibsn;
         this.titre = titre;
         this.Auteur = Auteur;
         this.nomEditeur = nomEditeur;
         this.dateParution = dateParution;
+        this.dernierExemplaire = 0 ;
+        this.exemplaires = new Set<Exemplaire>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Exemplaire> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Exemplaire exemplaire) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Exemplaire> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+        };
     }
 
 
