7de153150981d61b8645bf0bffa522c8b33dce44
jade <jadeclemenceaub@hotmail.com>
2022-03-17 10:31:03 +0100

    Ajout de la classe Exemplaire

--- src/main/java/modele/Exemplaire.java
+++ src/main/java/modele/Exemplaire.java
@@ -1,8 +1,25 @@
 package modele;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Exemplaire {
+public class Exemplaire implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;  // nécessaire pour la sérialisation
+    private Integer numExemp;
+    private LocalDate dateRecepExemp;
+    private Boolean empruntab;
+    private Ouvrage ouvrage;
+
+    public Exemplaire(Integer numExemp, LocalDate dateRecepExemp, Boolean empruntab, Ouvrage o) {
+        this.numExemp = numExemp;
+        this.dateRecepExemp = dateRecepExemp;
+        this.empruntab = empruntab;
+        this.ouvrage = o;
+    }
 }
+
+
